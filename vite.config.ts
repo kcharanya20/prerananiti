8f6578498d4eaf0874c8e20d06f45081803b1604
Charanya K Naik <charanya975@gmail.com>
2026-02-04 23:26:01 +0530

    Fix Netlify blank page (vite base path)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",              // ✅ ADD THIS LINE
   plugins: [react()],
   define: {
     // This allows process.env.API_KEY to work in the browser
